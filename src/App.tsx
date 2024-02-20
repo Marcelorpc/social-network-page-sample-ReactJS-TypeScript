6eae34e866cfe5209e2283c233bb602ea030d2f4
marcelorpc <marcelorpchaves@gmail.com>
2024-02-20 14:09:50 -0300

    vercel deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './css/global.css'
 
 import styles from "./css/App.module.css"
 import { Header } from "./components/Header"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 // author: {avatar_url: "", name: "", role: ""}
 //publishedAt: Date
@@ -55,6 +57,7 @@ function App() {
               post={post}
             />)
           })}
+          <SpeedInsights />
         </div>
       </main>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SpeedInsights />
     <App />
   </React.StrictMode>,
 )
